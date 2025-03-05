06fd2caf33468989212c5c985abba3e22bce418a
5harath <shartja@gmail.com>
2025-03-05 13:43:59 +0530

    Third commit

--- splitwise-frontend/src/index.js
+++ splitwise-frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import UserRegistration from './components/UserRegistration';
 
 const App = () => (
@@ -8,4 +8,6 @@ const App = () => (
     </div>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
